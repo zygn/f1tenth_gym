276edab5dc6186c0c6719417b37361bab596e401
botforge <dhruvkar@seas.upenn.edu>
2020-05-11 06:55:10 -0400

    gen dataset

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,13 +1,41 @@
 import numpy as np
 from wrappers import EgoCameraCar
 from fgm import FGM
+import os
 import pickle
+import matplotlib.pyplot as plt
 import pdb
 
-RENDER = False
-FOLDERPATH = './dataset'
-env = EgoCameraCar()
-agent = FGM(env.angle_min, env.angle_inc)
+"""
+obs.keys() = (['ego_idx', 'scans', 'poses_x', 'poses_y', 'poses_theta', 'linear_vels_x', 'linear_vels_y', 'ang_vels_z', 'collisions', 'collision_angles', 'lap_times', 'lap_counts', 'img'])
+"""
 
+RENDER = False
+FOLDERPATH = '../dataset'
+num_saves = 0
+
+def to_deg(rad):
+    return rad * 180/np.pi
+
+def save_data(obs, action):
+    global num_saves
+    if(num_saves == 0 and not os.path.exists(FOLDERPATH)):
+        os.mkdir(FOLDERPATH)
+    pkl_dict = {"obs":obs, "action":action}
+    filename = f"{FOLDERPATH}/sim_{num_saves}.pkl"
+    with open(filename, 'wb') as f:
+        pickle.dump(pkl_dict, f)
+    num_saves+=1
+
+env = EgoCameraCar()
+ego_agent = FGM(env.angle_min, env.angle_inc, speed=5.0)
 obs = env.reset()
-pdb.set_trace()
+while True:
+    #1) Use FGM to get action and save data
+    angle, speed = ego_agent.do_FGM(obs['scans'])
+    action = {'ego_idx':0, 'speed':[speed, 0.0], 'steer':[angle, 0.0]}
+    saved_action = {'angle': to_deg(angle), 'speed': speed}
+    save_data(obs, saved_action)
+
+    #2) Step the env
+    obs, rew, _, info = env.step(action)
--- scripts/wrappers.py
+++ scripts/wrappers.py
@@ -36,19 +36,22 @@ class DefaultCar(object):
         self.initial_state = {'x':[0., 2.0 if spawn_opp else 2000.], 'y': [0.0, 0.0], 'theta': [0., 0.0]}
         # self.obs, _, self.done, _ = self.racecar_env.reset(self.initial_state)
 
+    def _get_obs(self):
+        self.obs['scans'][0] = list(self.obs['scans'][0])
+        self.obs['scans'][1] = list(self.obs['scans'][1])
+        return self.obs
+
     def step(self, action):
         """
         ARGS: 
             action: {'ego_idx':0, 'speed':[ego_speed, opp_speed], 'steer':[ego_steer (rads), opp_steer (rads)]}
         """
         self.obs, step_reward, self.done, info = self.racecar_env.step(action)
-        self.obs['scans'][0] = list(self.obs['scans'][0])
-        self.obs['scans'][1] = list(self.obs['scans'][1])
-        return self.obs, step_reward, self.done, info
+        return self._get_obs(), step_reward, self.done, info
     
     def reset(self):
         self.obs, _, self.done, _ = self.racecar_env.reset(self.initial_state)
-        return self.obs
+        return self._get_obs()
 
 class DefaultCameraCar(DefaultCar):
     def __init__(self, spawn_opp=False, map_path='../maps/unreal.yaml', map_img_ext='.png', exec_dir='../build/', scan_fov=4.7, scan_beams=1080, scan_distance_to_base_link=0.275, csv_path='', cam_height=20.):
@@ -57,6 +60,7 @@ class DefaultCameraCar(DefaultCar):
 
         # TODO: HARCODED NOW - import unreal_origin from map.yaml
         unreal_origin = [88300.0, -40950.0, 6200.0]
+        unreal_origin[2] += cam_height
         self.cam = Camera(unreal_origin=unreal_origin)
     
     def ego_pose(self, obs):
