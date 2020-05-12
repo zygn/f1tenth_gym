2bca6fe78fddf929fd9b7172e29bed77cccab22d
botforge <dhruvkar@seas.upenn.edu>
2020-05-12 03:19:31 -0400

    new dataset

--- scripts/datasets.py
+++ scripts/datasets.py
@@ -37,3 +37,40 @@ class SteerDataset(Dataset):
         ts_angle = torch.Tensor([action.get("angle") * 180.0/math.pi]).float()
         ts_img = torch.from_numpy(cv_img).permute(2, 0, 1).float()
         return {"img":ts_img, "angle":ts_angle}
+    
+class SteerDataset_Class(Dataset):
+    from sklearn.cluster import KMeans
+    def __init__(self, folderpath, knn_model, transforms=None):
+        """
+        folderpath: String of directory containing pkl files
+        Each pkl should be a dictionary:
+        {
+            "obs": observation,
+            "action": action
+        }
+        """
+        super(SteerDataset_Class, self).__init__()
+        self.pkl_list = os.listdir(folderpath)
+        self.knn_model = knn_model
+        self.folderpath = folderpath
+
+    def __len__(self):
+        return len(self.pkl_list)
+
+    def __getitem__(self, idx):
+        """
+        Returns tuple (img- C x H x W Tensor, angle-float 1-Tensor)
+        """
+        pkl_name = self.pkl_list[idx]
+        with open(os.path.join(self.folderpath, pkl_name), 'rb') as f:
+            pkl_dict = pickle.load(f)
+
+        obs = pkl_dict.get("obs")
+        cv_img = obs.get("img")[:, :, :3]
+        cv_img = cv2.resize(cv_img, (0, 0), fx=0.5, fy=0.5)
+        ts_img = torch.from_numpy(cv_img).permute(2, 0, 1).float()
+
+        scans = np.array(obs.get("scans"))[None]
+        target = self.knn_model.fit(scans)
+        ts_target = torch.from_numpy(target)
+        return {"img":ts_img, "target":ts_target}
